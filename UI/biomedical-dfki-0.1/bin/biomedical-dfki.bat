167a5149df51859834e666f5b616ee031c21fcb8
Gaurav Vashisth <gaurav.vashisth@campus.tu-berlin.de>
2019-10-09 20:13:26 +0530

    new UI added

--- UI/biomedical-dfki-0.1/bin/biomedical-dfki.bat
+++ UI/biomedical-dfki-0.1/bin/biomedical-dfki.bat
@@ -9,25 +9,26 @@
 
 @echo off
 
-if "%BIOMEDICAL_DFKI_HOME%"=="" set "BIOMEDICAL_DFKI_HOME=%~dp0\\.."
 
-set "APP_LIB_DIR=%BIOMEDICAL_DFKI_HOME%\lib\"
+if "%BIOMEDICAL_DFKI_HOME%"=="" (
+  set "APP_HOME=%~dp0\\.."
+
+  rem Also set the old env name for backwards compatibility
+  set "BIOMEDICAL_DFKI_HOME=%~dp0\\.."
+) else (
+  set "APP_HOME=%BIOMEDICAL_DFKI_HOME%"
+)
+
+set "APP_LIB_DIR=%APP_HOME%\lib\"
 
 rem Detect if we were double clicked, although theoretically A user could
 rem manually run cmd /c
 for %%x in (!cmdcmdline!) do if %%~x==/c set DOUBLECLICKED=1
 
 rem FIRST we load the config file of extra options.
-set "CFG_FILE=%BIOMEDICAL_DFKI_HOME%\BIOMEDICAL_DFKI_config.txt"
+set "CFG_FILE=%APP_HOME%\BIOMEDICAL_DFKI_config.txt"
 set CFG_OPTS=
-if exist %CFG_FILE% (
-  FOR /F "tokens=* eol=# usebackq delims=" %%i IN ("%CFG_FILE%") DO (
-    set DO_NOT_REUSE_ME=%%i
-    rem ZOMG (Part #2) WE use !! here to delay the expansion of
-    rem CFG_OPTS, otherwise it remains "" for this loop.
-    set CFG_OPTS=!CFG_OPTS! !DO_NOT_REUSE_ME!
-  )
-)
+call :parse_config "%CFG_FILE%" CFG_OPTS
 
 rem We use the value of the JAVACMD environment variable if defined
 set _JAVACMD=%JAVACMD%
@@ -79,56 +80,16 @@ rem "-J" is stripped, "-D" is left as is, and everything is appended to JAVA_OPT
 set _JAVA_PARAMS=
 set _APP_ARGS=
 
-:param_loop
-call set _PARAM1=%%1
-set "_TEST_PARAM=%~1"
+set "APP_CLASSPATH=%APP_LIB_DIR%\..\conf\;%APP_LIB_DIR%\biomedical-dfki.biomedical-dfki-0.1-sans-externalized.jar;%APP_LIB_DIR%\rabbitmq-client.jar;%APP_LIB_DIR%\org.scala-lang.scala-library-2.13.0.jar;%APP_LIB_DIR%\com.typesafe.play.twirl-api_2.13-1.4.2.jar;%APP_LIB_DIR%\org.scala-lang.modules.scala-xml_2.13-1.2.0.jar;%APP_LIB_DIR%\com.typesafe.play.play-server_2.13-2.7.3.jar;%APP_LIB_DIR%\com.typesafe.play.play_2.13-2.7.3.jar;%APP_LIB_DIR%\com.typesafe.play.build-link-2.7.3.jar;%APP_LIB_DIR%\com.typesafe.play.play-exceptions-2.7.3.jar;%APP_LIB_DIR%\com.typesafe.play.play-streams_2.13-2.7.3.jar;%APP_LIB_DIR%\org.reactivestreams.reactive-streams-1.0.2.jar;%APP_LIB_DIR%\com.typesafe.akka.akka-stream_2.13-2.5.23.jar;%APP_LIB_DIR%\com.typesafe.akka.akka-actor_2.13-2.5.23.jar;%APP_LIB_DIR%\com.typesafe.config-1.3.3.jar;%APP_LIB_DIR%\org.scala-lang.modules.scala-java8-compat_2.13-0.9.0.jar;%APP_LIB_DIR%\com.typesafe.akka.akka-protobuf_2.13-2.5.23.jar;%APP_LIB_DIR%\com.typesafe.ssl-config-core_2.13-0.4.0.jar;%APP_LIB_DIR%\org.scala-lang.modules.scala-parser-combinators_2.13-1.1.2.jar;%APP_LIB_DIR%\org.slf4j.slf4j-api-1.7.26.jar;%APP_LIB_DIR%\org.slf4j.jul-to-slf4j-1.7.26.jar;%APP_LIB_DIR%\org.slf4j.jcl-over-slf4j-1.7.26.jar;%APP_LIB_DIR%\com.typesafe.akka.akka-slf4j_2.13-2.5.23.jar;%APP_LIB_DIR%\com.fasterxml.jackson.core.jackson-core-2.9.8.jar;%APP_LIB_DIR%\com.fasterxml.jackson.core.jackson-annotations-2.9.8.jar;%APP_LIB_DIR%\com.fasterxml.jackson.core.jackson-databind-2.9.8.jar;%APP_LIB_DIR%\com.fasterxml.jackson.datatype.jackson-datatype-jdk8-2.9.8.jar;%APP_LIB_DIR%\com.fasterxml.jackson.datatype.jackson-datatype-jsr310-2.9.8.jar;%APP_LIB_DIR%\com.typesafe.play.play-json_2.13-2.7.4.jar;%APP_LIB_DIR%\com.typesafe.play.play-functional_2.13-2.7.4.jar;%APP_LIB_DIR%\org.scala-lang.scala-reflect-2.13.0.jar;%APP_LIB_DIR%\joda-time.joda-time-2.10.1.jar;%APP_LIB_DIR%\com.google.guava.guava-27.1-jre.jar;%APP_LIB_DIR%\com.google.guava.failureaccess-1.0.1.jar;%APP_LIB_DIR%\com.google.guava.listenablefuture-9999.0-empty-to-avoid-conflict-with-guava.jar;%APP_LIB_DIR%\com.google.code.findbugs.jsr305-3.0.2.jar;%APP_LIB_DIR%\org.checkerframework.checker-qual-2.5.2.jar;%APP_LIB_DIR%\com.google.errorprone.error_prone_annotations-2.2.0.jar;%APP_LIB_DIR%\com.google.j2objc.j2objc-annotations-1.1.jar;%APP_LIB_DIR%\org.codehaus.mojo.animal-sniffer-annotations-1.17.jar;%APP_LIB_DIR%\io.jsonwebtoken.jjwt-0.9.1.jar;%APP_LIB_DIR%\javax.xml.bind.jaxb-api-2.3.1.jar;%APP_LIB_DIR%\javax.activation.javax.activation-api-1.2.0.jar;%APP_LIB_DIR%\javax.transaction.jta-1.1.jar;%APP_LIB_DIR%\javax.inject.javax.inject-1.jar;%APP_LIB_DIR%\com.typesafe.play.play-logback_2.13-2.7.3.jar;%APP_LIB_DIR%\ch.qos.logback.logback-classic-1.2.3.jar;%APP_LIB_DIR%\ch.qos.logback.logback-core-1.2.3.jar;%APP_LIB_DIR%\com.typesafe.play.play-akka-http-server_2.13-2.7.3.jar;%APP_LIB_DIR%\com.typesafe.akka.akka-http-core_2.13-10.1.8.jar;%APP_LIB_DIR%\com.typesafe.akka.akka-parsing_2.13-10.1.8.jar;%APP_LIB_DIR%\com.typesafe.play.filters-helpers_2.13-2.7.3.jar;%APP_LIB_DIR%\com.typesafe.play.play-jdbc_2.13-2.7.3.jar;%APP_LIB_DIR%\com.typesafe.play.play-jdbc-api_2.13-2.7.3.jar;%APP_LIB_DIR%\com.zaxxer.HikariCP-3.3.1.jar;%APP_LIB_DIR%\com.googlecode.usc.jdbcdslog-1.0.6.2.jar;%APP_LIB_DIR%\tyrex.tyrex-1.0.1.jar;%APP_LIB_DIR%\com.typesafe.play.play-ehcache_2.13-2.7.3.jar;%APP_LIB_DIR%\com.typesafe.play.play-cache_2.13-2.7.3.jar;%APP_LIB_DIR%\net.sf.ehcache.ehcache-2.10.6.jar;%APP_LIB_DIR%\org.ehcache.jcache-1.0.1.jar;%APP_LIB_DIR%\javax.cache.cache-api-1.0.0.jar;%APP_LIB_DIR%\com.typesafe.play.play-ahc-ws_2.13-2.7.3.jar;%APP_LIB_DIR%\com.typesafe.play.play-ws_2.13-2.7.3.jar;%APP_LIB_DIR%\com.typesafe.play.play-ws-standalone_2.13-2.0.6.jar;%APP_LIB_DIR%\com.typesafe.play.play-ws-standalone-xml_2.13-2.0.6.jar;%APP_LIB_DIR%\com.typesafe.play.play-ws-standalone-json_2.13-2.0.6.jar;%APP_LIB_DIR%\com.typesafe.play.play-ahc-ws-standalone_2.13-2.0.6.jar;%APP_LIB_DIR%\com.typesafe.play.cachecontrol_2.13-1.1.7.jar;%APP_LIB_DIR%\org.joda.joda-convert-1.9.2.jar;%APP_LIB_DIR%\com.typesafe.play.shaded-asynchttpclient-2.0.6.jar;%APP_LIB_DIR%\com.typesafe.play.shaded-oauth-2.0.6.jar;%APP_LIB_DIR%\com.typesafe.play.play-guice_2.13-2.7.3.jar;%APP_LIB_DIR%\com.google.inject.guice-4.2.2.jar;%APP_LIB_DIR%\aopalliance.aopalliance-1.0.jar;%APP_LIB_DIR%\com.google.inject.extensions.guice-assistedinject-4.2.2.jar;%APP_LIB_DIR%\biomedical-dfki.biomedical-dfki-0.1-assets.jar"
+set "APP_MAIN_CLASS=play.core.server.ProdServerStart"
+set "SCRIPT_CONF_FILE=%APP_HOME%\conf\application.ini"
 
-if ["!_PARAM1!"]==[""] goto param_afterloop
+rem if configuration files exist, prepend their contents to the script arguments so it can be processed by this runner
+call :parse_config "%SCRIPT_CONF_FILE%" SCRIPT_CONF_ARGS
 
-
-rem ignore arguments that do not start with '-'
-if "%_TEST_PARAM:~0,1%"=="-" goto param_java_check
-set _APP_ARGS=!_APP_ARGS! !_PARAM1!
-shift
-goto param_loop
-
-:param_java_check
-if "!_TEST_PARAM:~0,2!"=="-J" (
-  rem strip -J prefix
-  set _JAVA_PARAMS=!_JAVA_PARAMS! !_TEST_PARAM:~2!
-  shift
-  goto param_loop
-)
-
-if "!_TEST_PARAM:~0,2!"=="-D" (
-  rem test if this was double-quoted property "-Dprop=42"
-  for /F "delims== tokens=1,*" %%G in ("!_TEST_PARAM!") DO (
-    if not ["%%H"] == [""] (
-      set _JAVA_PARAMS=!_JAVA_PARAMS! !_PARAM1!
-    ) else if [%2] neq [] (
-      rem it was a normal property: -Dprop=42 or -Drop="42"
-      call set _PARAM1=%%1=%%2
-      set _JAVA_PARAMS=!_JAVA_PARAMS! !_PARAM1!
-      shift
-    )
-  )
-) else (
-  if "!_TEST_PARAM!"=="-main" (
-    call set CUSTOM_MAIN_CLASS=%%2
-    shift
-  ) else (
-    set _APP_ARGS=!_APP_ARGS! !_PARAM1!
-  )
-)
-shift
-goto param_loop
-:param_afterloop
+call :process_args %SCRIPT_CONF_ARGS% %%*
 
 set _JAVA_OPTS=!_JAVA_OPTS! !_JAVA_PARAMS!
-:run
- 
-set "APP_CLASSPATH=%APP_LIB_DIR%\..\conf\;%APP_LIB_DIR%\biomedical-dfki.biomedical-dfki-0.1-sans-externalized.jar;%APP_LIB_DIR%\rabbitmq-client.jar;%APP_LIB_DIR%\org.scala-lang.scala-library-2.11.6.jar;%APP_LIB_DIR%\com.typesafe.play.twirl-api_2.11-1.1.1.jar;%APP_LIB_DIR%\org.apache.commons.commons-lang3-3.4.jar;%APP_LIB_DIR%\org.scala-lang.modules.scala-xml_2.11-1.0.1.jar;%APP_LIB_DIR%\com.typesafe.play.play-server_2.11-2.4.3.jar;%APP_LIB_DIR%\com.typesafe.play.play_2.11-2.4.3.jar;%APP_LIB_DIR%\com.typesafe.play.build-link-2.4.3.jar;%APP_LIB_DIR%\com.typesafe.play.play-exceptions-2.4.3.jar;%APP_LIB_DIR%\org.javassist.javassist-3.19.0-GA.jar;%APP_LIB_DIR%\com.typesafe.play.play-iteratees_2.11-2.4.3.jar;%APP_LIB_DIR%\org.scala-stm.scala-stm_2.11-0.7.jar;%APP_LIB_DIR%\com.typesafe.config-1.3.0.jar;%APP_LIB_DIR%\com.typesafe.play.play-json_2.11-2.4.3.jar;%APP_LIB_DIR%\com.typesafe.play.play-functional_2.11-2.4.3.jar;%APP_LIB_DIR%\com.typesafe.play.play-datacommons_2.11-2.4.3.jar;%APP_LIB_DIR%\joda-time.joda-time-2.8.1.jar;%APP_LIB_DIR%\org.joda.joda-convert-1.7.jar;%APP_LIB_DIR%\org.scala-lang.scala-reflect-2.11.6.jar;%APP_LIB_DIR%\com.fasterxml.jackson.core.jackson-core-2.5.4.jar;%APP_LIB_DIR%\com.fasterxml.jackson.core.jackson-annotations-2.5.4.jar;%APP_LIB_DIR%\com.fasterxml.jackson.core.jackson-databind-2.5.4.jar;%APP_LIB_DIR%\com.fasterxml.jackson.datatype.jackson-datatype-jdk8-2.5.4.jar;%APP_LIB_DIR%\com.fasterxml.jackson.datatype.jackson-datatype-jsr310-2.5.4.jar;%APP_LIB_DIR%\com.typesafe.play.play-netty-utils-2.4.3.jar;%APP_LIB_DIR%\org.slf4j.slf4j-api-1.7.12.jar;%APP_LIB_DIR%\org.slf4j.jul-to-slf4j-1.7.12.jar;%APP_LIB_DIR%\org.slf4j.jcl-over-slf4j-1.7.12.jar;%APP_LIB_DIR%\org.scala-lang.modules.scala-parser-combinators_2.11-1.0.1.jar;%APP_LIB_DIR%\ch.qos.logback.logback-core-1.1.3.jar;%APP_LIB_DIR%\ch.qos.logback.logback-classic-1.1.3.jar;%APP_LIB_DIR%\com.typesafe.akka.akka-actor_2.11-2.3.13.jar;%APP_LIB_DIR%\com.typesafe.akka.akka-slf4j_2.11-2.3.13.jar;%APP_LIB_DIR%\commons-codec.commons-codec-1.10.jar;%APP_LIB_DIR%\xerces.xercesImpl-2.11.0.jar;%APP_LIB_DIR%\xml-apis.xml-apis-1.4.01.jar;%APP_LIB_DIR%\javax.transaction.jta-1.1.jar;%APP_LIB_DIR%\com.google.inject.guice-4.0.jar;%APP_LIB_DIR%\javax.inject.javax.inject-1.jar;%APP_LIB_DIR%\aopalliance.aopalliance-1.0.jar;%APP_LIB_DIR%\com.google.inject.extensions.guice-assistedinject-4.0.jar;%APP_LIB_DIR%\com.typesafe.play.play-netty-server_2.11-2.4.3.jar;%APP_LIB_DIR%\io.netty.netty-3.10.4.Final.jar;%APP_LIB_DIR%\com.typesafe.netty.netty-http-pipelining-1.1.4.jar;%APP_LIB_DIR%\com.typesafe.play.play-jdbc_2.11-2.4.3.jar;%APP_LIB_DIR%\com.typesafe.play.play-jdbc-api_2.11-2.4.3.jar;%APP_LIB_DIR%\com.jolbox.bonecp-0.8.0.RELEASE.jar;%APP_LIB_DIR%\com.zaxxer.HikariCP-2.3.7.jar;%APP_LIB_DIR%\com.h2database.h2-1.4.187.jar;%APP_LIB_DIR%\tyrex.tyrex-1.0.1.jar;%APP_LIB_DIR%\com.typesafe.play.play-cache_2.11-2.4.3.jar;%APP_LIB_DIR%\net.sf.ehcache.ehcache-core-2.6.11.jar;%APP_LIB_DIR%\com.typesafe.play.play-ws_2.11-2.4.3.jar;%APP_LIB_DIR%\com.google.guava.guava-18.0.jar;%APP_LIB_DIR%\com.ning.async-http-client-1.9.21.jar;%APP_LIB_DIR%\oauth.signpost.signpost-core-1.2.1.2.jar;%APP_LIB_DIR%\oauth.signpost.signpost-commonshttp4-1.2.1.2.jar;%APP_LIB_DIR%\org.apache.httpcomponents.httpcore-4.0.1.jar;%APP_LIB_DIR%\org.apache.httpcomponents.httpclient-4.0.1.jar;%APP_LIB_DIR%\commons-logging.commons-logging-1.1.1.jar;%APP_LIB_DIR%\biomedical-dfki.biomedical-dfki-0.1-assets.jar"
-set "APP_MAIN_CLASS=play.core.server.ProdServerStart"
 
 if defined CUSTOM_MAIN_CLASS (
     set MAIN_CLASS=!CUSTOM_MAIN_CLASS!
@@ -141,7 +102,79 @@ rem Call the application and pass all arguments unchanged.
 
 @endlocal
 
-
-:end
-
 exit /B %ERRORLEVEL%
+
+
+rem Loads a configuration file full of default command line options for this script.
+rem First argument is the path to the config file.
+rem Second argument is the name of the environment variable to write to.
+:parse_config
+  set _PARSE_FILE=%~1
+  set _PARSE_OUT=
+  if exist "%_PARSE_FILE%" (
+    FOR /F "tokens=* eol=# usebackq delims=" %%i IN ("%_PARSE_FILE%") DO (
+      set _PARSE_OUT=!_PARSE_OUT! %%i
+    )
+  )
+  set %2=!_PARSE_OUT!
+exit /B 0
+
+
+:add_java
+  set _JAVA_PARAMS=!_JAVA_PARAMS! %*
+exit /B 0
+
+
+:add_app
+  set _APP_ARGS=!_APP_ARGS! %*
+exit /B 0
+
+
+rem Processes incoming arguments and places them in appropriate global variables
+:process_args
+  :param_loop
+  call set _PARAM1=%%1
+  set "_TEST_PARAM=%~1"
+
+  if ["!_PARAM1!"]==[""] goto param_afterloop
+
+
+  rem ignore arguments that do not start with '-'
+  if "%_TEST_PARAM:~0,1%"=="-" goto param_java_check
+  set _APP_ARGS=!_APP_ARGS! !_PARAM1!
+  shift
+  goto param_loop
+
+  :param_java_check
+  if "!_TEST_PARAM:~0,2!"=="-J" (
+    rem strip -J prefix
+    set _JAVA_PARAMS=!_JAVA_PARAMS! !_TEST_PARAM:~2!
+    shift
+    goto param_loop
+  )
+
+  if "!_TEST_PARAM:~0,2!"=="-D" (
+    rem test if this was double-quoted property "-Dprop=42"
+    for /F "delims== tokens=1,*" %%G in ("!_TEST_PARAM!") DO (
+      if not ["%%H"] == [""] (
+        set _JAVA_PARAMS=!_JAVA_PARAMS! !_PARAM1!
+      ) else if [%2] neq [] (
+        rem it was a normal property: -Dprop=42 or -Drop="42"
+        call set _PARAM1=%%1=%%2
+        set _JAVA_PARAMS=!_JAVA_PARAMS! !_PARAM1!
+        shift
+      )
+    )
+  ) else (
+    if "!_TEST_PARAM!"=="-main" (
+      call set CUSTOM_MAIN_CLASS=%%2
+      shift
+    ) else (
+      set _APP_ARGS=!_APP_ARGS! !_PARAM1!
+    )
+  )
+  shift
+  goto param_loop
+  :param_afterloop
+
+exit /B 0
